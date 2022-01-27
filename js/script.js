197318e10910587b3af6db7347273048e779b22f
Crispy-rw <nshimyumukizachristian@gmail.com>
2022-01-27 13:32:41 +0200

    Last Commit

--- js/script.js
+++ js/script.js
@@ -12,20 +12,32 @@ $(document).ready(() => {
 
   let phone_focused = false;
 
-  phone.focus(function () {
-    phone_focused = true;
-  });
-
   // Validate Phone input on change
-  phone.keydown(function (e) {
-    phone_focused = true;
-    if (testPhoneLen()) {
-      $('#phone-error').css('visibility', 'visible');
-      phone.addClass('border-red');
-      phone.addClass('red');
+  phone.keydown(function (event) {
+    if (event.shiftKey == true) {
+      event.preventDefault();
+    }
+
+    if (
+      (event.keyCode >= 48 && event.keyCode <= 57) ||
+      (event.keyCode >= 96 && event.keyCode <= 105) ||
+      event.keyCode == 8 ||
+      event.keyCode == 9 ||
+      event.keyCode == 37 ||
+      event.keyCode == 39 ||
+      event.keyCode == 46 ||
+      event.keyCode == 190
+    ) {
+      if (testPhoneLen()) {
+        $('#phone-error').css('visibility', 'visible');
+        phone.addClass('border-red');
+        phone.addClass('red');
+      } else {
+        phone.removeClass('red');
+        $('#phone-error').css('visibility', 'hidden');
+      }
     } else {
-      phone.removeClass('red');
-      $('#phone-error').css('visibility', 'hidden');
+      event.preventDefault();
     }
   });
 
